d8551af06647507f9521106e4d74494f4e694438
Austin Luft <ausluft@gmail.com>
2018-09-21 21:52:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(array)
+  badge_messages = []
+  array.each do |n|
+    badge_messages.push(badge_maker(n))
+  end
+  return badge_messages
+end
+
+
+def assign_rooms(array)
+  array.each_with_index { |n, x| puts "Hello, #{n}! You'll be assigned to room #{x + 1}!"}
+end
+
+def printer
+  
+end
