90f254e4d0da6de4905961287982f36a7cfadcbe
Austin Luft <ausluft@gmail.com>
2018-09-21 22:06:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,7 @@ def assign_rooms(array)
   return room_messages
 end
 
-def printer(array)
-  puts batch_badge_creator(array)
+def printer(method)
+  batch_badge_creator(array)
   puts assign_rooms(array)
 end
